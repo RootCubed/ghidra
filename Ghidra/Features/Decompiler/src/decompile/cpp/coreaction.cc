d823b8c5faed80db5d8177d6a03cf2e8ead4086e
Grop <broccolutje@gmail.com>
2022-05-22 21:26:35 +0200

    Clean up sign bit-based comparisons

--- Ghidra/Features/Decompiler/src/decompile/cpp/coreaction.cc
+++ Ghidra/Features/Decompiler/src/decompile/cpp/coreaction.cc
@@ -4,9 +4,9 @@
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
- * 
+ *
  *      http://www.apache.org/licenses/LICENSE-2.0
- * 
+ *
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
@@ -5140,6 +5140,8 @@ void ActionDatabase::universalAction(Architecture *conf)
     actcleanup->addRule( new RuleSubRight("cleanup") );
     actcleanup->addRule( new RulePtrsubCharConstant("cleanup") );
     actcleanup->addRule( new RuleExtensionPush("cleanup") );
+    actcleanup->addRule( new RuleCleanSignBitCompare("cleanup") );
+    actcleanup->addRule( new RuleCleanBoolSubpiece("cleanup") );
   }
   act->addAction( actcleanup );
 
